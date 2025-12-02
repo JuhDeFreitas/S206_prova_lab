fa0525d8fd4b378a8a32426ba363f7ad5ee14072
JuhDeFreita <julia.carvalho@gec.inatel.br>
2025-12-02 01:11:09 -0300

    chore: correção no README

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -1,7 +1,19 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/html",
+      overwrite: true,
+      html: true,
+      json: false
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
+      // hooks se precisar
+    }
+  }
+});
